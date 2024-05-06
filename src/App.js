f249458a6826a42931ae14f72b31c732fa034ae9
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-05-06 10:55:39 +0300

    trial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,Switch} from 'react-router-dom';
 import BotSpecs from './components/BotSpecs';
 
 function App() {
@@ -10,10 +10,10 @@ function App() {
     <Router>
       <div className="App">
         <h1>Bot Battlr</h1>
-        <Switch>
+        <Routes>
           <Route path="/" exact component={BotCollection} />
           <Route path="/bots/:botId" component={BotSpecs} />
-        </Switch>
+        </Routes>
         <YourBotArmy />
       </div>
     </Router>
